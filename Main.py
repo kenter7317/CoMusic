6442fb08583dfdc17904c9d067554b0398e54a5b
kenter7317 <kenter7317@gmail.com>
2025-06-16 06:27:30 +0900

    gui init default gui scheme init

--- Main.py
+++ Main.py
@@ -1,15 +1,41 @@
-from PyQt5.QtCore import QUrlQuery, QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QUrlQuery, QUrl, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QListWidget, QListView, QDockWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import sys
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-browser = QWebEngineView()
-url = QUrl('http://google.com') # will change YouTube Embed
+# Window Scheme
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        browser = QWebEngineView()
+        url = QUrl('http://google.com') # will change YouTube Embed
 
-browser.load(url)
-window.setCentralWidget(browser)
-window.resize(1024, 768)
+        menu_bar = self.menuBar()
+        menu_file = menu_bar.addMenu("파일")
+
+        exit_action = QAction("종료", self)
+        exit_action.triggered.connect(self.close)
+        menu_file.addAction(exit_action)
+
+
+        list_dock = QDockWidget(self)
+        list_widget = QListView(self)
+        list_dock.setWidget(list_widget)
+        list_dock.setFloating(True)
+        list_dock.hide()
+        list_dock.setAllowedAreas(Qt.AllDockWidgetAreas)
+        list_action = QAction("리스트", self)
+        list_action.triggered.connect(lambda: list_dock.show())
+
+        menu_file.addAction(list_action)
+
+        browser.load(url)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setCentralWidget(browser)
+        self.resize(1024, 768)
+
+app = QApplication(sys.argv)
+window = MyWindow()
 window.show()
 sys.exit(app.exec_())
