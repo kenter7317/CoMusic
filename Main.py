441001b3c0c7f514db27155e484eba2f377e5b15
kenter7317 <kenter7317@gmail.com>
2025-06-16 07:42:03 +0900

    gui update add Local Youtube embed html, todo: webchannel connection, List apply

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt5.QtCore import QUrlQuery, QUrl, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QListWidget, QListView, QDockWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
@@ -8,7 +10,8 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         browser = QWebEngineView()
-        url = QUrl('http://google.com') # will change YouTube Embed
+        html_path = os.path.abspath("Embed.html") # TODO WEBCHANNEL
+        url = QUrl.fromLocalFile(html_path)
 
         menu_bar = self.menuBar()
         menu_file = menu_bar.addMenu("파일")
@@ -16,7 +19,6 @@ class MyWindow(QMainWindow):
         exit_action = QAction("종료", self)
         exit_action.triggered.connect(self.close)
         menu_file.addAction(exit_action)
-
 
         list_dock = QDockWidget(self)
         list_widget = QListView(self)
@@ -28,13 +30,14 @@ class MyWindow(QMainWindow):
         list_action.triggered.connect(lambda: list_dock.show())
 
         menu_file.addAction(list_action)
-
         browser.load(url)
 
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setCentralWidget(browser)
         self.resize(1024, 768)
 
+class DiscordBot():
+    def __init__(self): # TODO
 app = QApplication(sys.argv)
 window = MyWindow()
 window.show()
